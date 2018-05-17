9a55a92fc7e5769561e3393dc200e9ee3d013427
ChriswFoster <chriswfoster@gmail.com>
2018-05-17 16:22:16 -0500

    Adding images to project

--- src/components/routedchildren/Projects/Projects.js
+++ src/components/routedchildren/Projects/Projects.js
@@ -2,12 +2,52 @@ import React, { Component } from "react"
 import './projects.css'
 class Projects extends Component {
   render() {
-    return <div> 
+    return <div className="contact-main">
       <div className="contact-topbar">
           <p> PORTFOLIO. </p>
           <p> Some of my recent projects </p>
         </div>
-
+    <div>
+    <a
+              href="http://dream.chriswf.com"
+              style={{ textDecoration: "none" }}
+              className="projectlinks"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("./DreamScreenshot.png")}
+                className="projectpictiles" alt="imgz"
+              />
+              <h2>Dream Notes</h2>
+            </a>
+            <a
+              href="http://reactgiphy.chriswf.com"
+              style={{ textDecoration: "none" }}
+              className="projectlinks"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("./GiphyScreenshot.png")}
+                className="projectpictiles" alt="imgz"
+              />
+              <h2>Giphy Search Tool (React Variant)</h2>
+            </a>
+            <a
+              href="https://www.youtube.com/watch?v=xsKCZNNVW8o&feature=youtu.be"
+              style={{ textDecoration: "none" }}
+              className="projectlinks"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={require("./QueueScreenshot.png")}
+                className="projectpictiles" alt="imgz"
+              />
+                 <h2>Student Help Queue</h2>
+            </a>
+      </div>
 
     </div>
   }
