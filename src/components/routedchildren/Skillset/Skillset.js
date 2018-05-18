701b8611fa0f7a540cbfdf3d435160780b42e1ed
ChriswFoster <chriswfoster@gmail.com>
2018-05-17 20:32:34 -0500

    skill icons added

--- src/components/routedchildren/Skillset/Skillset.js
+++ src/components/routedchildren/Skillset/Skillset.js
@@ -1,13 +1,15 @@
-import React, {Component} from 'react';
-import './skillset.css'
+import React, { Component } from "react"
+import "./skillset.css"
 class Skillset extends Component {
-
-
-render() {
-return(
-<div>
-    Skillset
-</div>
-)}
+  render() {
+    return (
+      <div className="contact-main">
+        <div className="contact-topbar"> 
+        <p>SKILLSET</p>
+        <p>Some of the skills and tech that I have knowledge in.</p>
+        </div>
+      </div>
+    )
+  }
 }
 export default Skillset
