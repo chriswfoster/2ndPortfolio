95dcecf80dba55eff35753280df339895ac2bd60
Chris <chriswfoster@gmail.com>
2018-05-21 15:00:34 -0500

    Reworked, functional landing

--- src/components/App.js
+++ src/components/App.js
@@ -19,62 +19,168 @@ import "./App.css"
 import "./font-awesome/css/font-awesome.min.css"
 
 class App extends Component {
-  // we need to map the `scale` prop we define below
-  // to the transform style property
-  mapStyles(styles) {
-    return {
-      opacity: styles.opacity,
-      transform: `scale(${styles.scale})`
+  constructor() {
+    super()
+    this.state = {
+      topleft: null,
+      topright: null,
+      bottomleft: null,
+      bottomright: null
     }
   }
 
-  // wrap the `spring` helper to use a bouncy config
-  bounce(val) {
-    return spring(val, {
-      stiffness: 330,
-      damping: 22
+  leave() {
+    this.setState({
+      topleft: null,
+      topright: null,
+      bottomleft: null,
+      bottomright: null
     })
   }
 
-  bounceTransition = {
-    // start in a transparent, upscaled state
-    atEnter: {
-      opacity: 0,
-      scale: 1.2
-    },
-    // leave in a transparent, downscaled state
-    atLeave: {
-      opacity: this.bounce(0),
-      scale: this.bounce(0.8)
-    },
-    // and rest at an opaque, normally-scaled state
-    atActive: {
-      opacity: this.bounce(1),
-      scale: this.bounce(1)
+  expand(side, side2, side3, side4) {
+    if (this.state[side] === side) {
+      return { width: "85vw", height: "85vh", zIndex: "3" }
+    } else if (
+      this.state[side] === side2 ||
+      this.state[side] === side3 ||
+      this.state[side] === side4
+    ) {
+      return { width: "30vw", height: "30vh", opacity: "0.3", zIndex: "2" }
     }
+    return { width: "45vw", height: "45vh" }
+  }
+  size(first, second, third, fourth) {
+    this.setState({
+      [first]: first,
+      [second]: first,
+      [third]: first,
+      [fourth]: first
+    })
   }
 
   render() {
     return (
       <div className="App-main">
+
         <p className="App-firstname font-righteous">CHRIS</p>
         <p className="App-lastname font-righteous">FOSTER</p>
-        <div className="App-aboutme App-tilesize "> About Me </div>
-        <div className="App-projects App-tilesize "> Projects </div>
-        <div className="App-skillset App-tilesize "> Skillset </div>
-        <div className="App-contact App-tilesize "> Contact </div>
+        <div
+          className="App-aboutme App-tilesize "
+          style={this.expand(
+            "topleft",
+            "topright",
+            "bottomleft",
+            "bottomright"
+          )}
+          onMouseEnter={() =>
+            this.size("topleft", "topright", "bottomleft", "bottomright")
+          }
+          onMouseLeave={() => this.leave()}
+        >
+          <div className="App-innertile">
+            <p>About Me</p>
+          </div>
+        </div>
+        <div
+          className="App-projects App-tilesize "
+          style={this.expand(
+            "topright",
+            "topleft",
+            "bottomleft",
+            "bottomright"
+          )}
+          onMouseEnter={() =>
+            this.size("topright", "topleft", "bottomleft", "bottomright")
+          }
+          onMouseLeave={() => this.leave()}
+        >
+          <div className="App-innertile">
+            <p>Projects</p>
+          </div>
+        </div>
+        <div
+          className="App-skillset App-tilesize "
+          style={this.expand(
+            "bottomleft",
+            "bottomright",
+            "topleft",
+            "topright"
+          )}
+          onMouseEnter={() =>
+            this.size("bottomleft", "bottomright", "topleft", "topright")
+          }
+          onMouseLeave={() => this.leave()}
+        >
+          <div className="App-innertile">
+            <p>Skillset</p>
+          </div>
+        </div>
+        <div
+          className="App-contact App-tilesize "
+          style={this.expand(
+            "bottomright",
+            "bottomleft",
+            "topright",
+            "topleft"
+          )}
+          onMouseEnter={() =>
+            this.size("bottomright", "bottomleft", "topright", "topleft")
+          }
+          onMouseLeave={() => this.leave()}
+        >
+          <div className="App-innertile">
+            <p>Contact</p>
+          </div>
+        </div>
         <p className="App-fullstack font-righteous App-vertialtext">
           FULL STACK
         </p>
         <p className="App-webdeveloper font-righteous App-vertialtext">
           WEB DEVELOPER
         </p>
+        
       </div>
     )
   }
 }
 
 export default App
+
+//  // we need to map the `scale` prop we define below
+//   // to the transform style property
+//   mapStyles(styles) {
+//     return {
+//       opacity: styles.opacity,
+//       transform: `scale(${styles.scale})`
+//     }
+//   }
+
+//   // wrap the `spring` helper to use a bouncy config
+//   bounce(val) {
+//     return spring(val, {
+//       stiffness: 330,
+//       damping: 22
+//     })
+//   }
+
+//   bounceTransition = {
+//     // start in a transparent, upscaled state
+//     atEnter: {
+//       opacity: 0,
+//       scale: 1.2
+//     },
+//     // leave in a transparent, downscaled state
+//     atLeave: {
+//       opacity: this.bounce(0),
+//       scale: this.bounce(0.8)
+//     },
+//     // and rest at an opaque, normally-scaled state
+//     atActive: {
+//       opacity: this.bounce(1),
+//       scale: this.bounce(1)
+//     }
+//   }
 
 {
   /* <AnimatedSwitch
