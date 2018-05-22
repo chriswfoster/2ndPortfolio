0b690d648863d64645a60970aab8abdac779571a
Chris <chriswfoster@gmail.com>
2018-05-22 16:58:34 -0500

    Cleaning warnings and errors

--- src/components/four-main-tiles/routedchildren/Contact/Contact.js
+++ src/components/four-main-tiles/routedchildren/Contact/Contact.js
@@ -1,27 +1,11 @@
 import React, { Component } from "react"
-import {
-  withScriptjs,
-  withGoogleMap,
-  GoogleMap,
-  Marker
-} from "react-google-maps"
+
 import ContactForm from "./ContactForm/ContactForm"
 import "./contact.css"
 
 class Contact extends Component {
   render() {
-    const MyMapComponent = withScriptjs(
-      withGoogleMap(props => (
-        <GoogleMap
-          defaultZoom={10}
-          defaultCenter={{ lat: 33.004086, lng: -96.708479 }}
-        >
-          {props.isMarkerShown && (
-            <Marker position={{ lat: 33.004086, lng: -96.708479 }} />
-          )}
-        </GoogleMap>
-      ))
-    )
+    
     return (
       <div className="contact-main" style={{ opacity: "1" }}>
         <div className="contact-topbar font-righteous ">
