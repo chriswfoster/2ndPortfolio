604dd20db089e2642d7ba0900d8e67ec6222025a
Chris <chriswfoster@gmail.com>
2018-05-22 16:56:39 -0500

    Cleaning warnings and errors

--- src/components/four-main-tiles/routedchildren/AboutMe/AboutMe.js
+++ src/components/four-main-tiles/routedchildren/AboutMe/AboutMe.js
@@ -1,12 +1,4 @@
 import React, { Component } from "react"
-import { TransitionGroup, CSSTransition } from "react-transition-group"
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect
-} from "react-router-dom"
 import "./home.css"
 
 class Home extends Component {
